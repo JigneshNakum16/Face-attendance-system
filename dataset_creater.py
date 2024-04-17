d257534b9b11cf04197099b1fe2aa0b77755becf
JigneshNakum16 <jigneshnakum16@gmail.com>
2024-04-17 16:41:02 +0530

    fixed bugs in dataset_creater file

--- dataset_creater.py
+++ dataset_creater.py
@@ -1,53 +1,56 @@
-import cv2      #OpenCv camera
+import cv2
 import numpy as np
 import sqlite3
-
+import os
 
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
 
+def insertToUpdate(id, name, age):
+    try:
+        conn = sqlite3.connect("sqlite.db")
+        cursor = conn.execute("SELECT * FROM students WHERE id = ?", (id,))
+        isRecordExists = cursor.fetchone()
 
-def insertToUpdate(id,name,age):
-    conn = sqlite3.connect("sqlite.db")
-    cmd = "select * from students where id = " + str(id)
+        if isRecordExists:
+            conn.execute("UPDATE students SET name = ?, age = ? WHERE id = ?", (name, age, id))
+        else:
+            conn.execute("INSERT INTO students (id, name, age) VALUES (?, ?, ?)", (id, name, age))
 
-    cursor = conn.execute(cmd)
-    isRecordExits = 0
+        conn.commit()
+    except sqlite3.Error as e:
+        print("SQLite error:", e)
+    finally:
+        conn.close()
 
-    for row in cursor:
-        isRecordExits = 1
-    
-    if(isRecordExits):
-        conn.execute("update students set name = ? where id = ?",(name,id))
-        conn.execute("update students set age = ? where id = ?",(age,id))
-    else:
-        conn.execute("insert into students (id,name,age) values (?, ?, ?)",(id,name,age))
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
+id = int(input("Enter the ID: "))
+name = input("Enter the name: ")
+age = int(input("Enter the age: "))
 
-    conn.commit()
-    conn.close()
+insertToUpdate(id, name, age)
 
-id = input("Enter the ID : ")
-name = input("Enter the name : ")
-age  = input("Enter the age : ")
-
-insertToUpdate(id,name,age)
-
-
-#detect face in web camera coding
+dataset_directory = "dataset"
+create_directory(dataset_directory)
 
 sampleNum = 0
-while(True):
-    ret,img = cam.read()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = faceDetect.detectMultiScale(gray,1.3,5)
-    for(x,y,w,h) in faces:
-        sampleNum = sampleNum + 1
-        cv2.imwrite("/dataset/user."+str(id) + "."+ str(sampleNum) + ".jpg",gray[y:y+h,x:x+w])
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.imshow("Face",img)
+while True:
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        sampleNum += 1
+        cv2.imwrite(os.path.join(dataset_directory, f"user.{id}.{sampleNum}.jpg"), gray[y:y+h, x:x+w])
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+    cv2.imshow("Face", img)
     cv2.waitKey(1)
-    if(sampleNum>20):
+    
+    if sampleNum > 20:
         break
 
 cam.release()
